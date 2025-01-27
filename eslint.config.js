654e5d69afeb2a44cca0751335316e05e8755b09
anakon <nazarkinah73@gmail.com>
2025-01-27 13:23:53 +0200

    chore: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,23 @@
 import antfu from '@antfu/eslint-config'
 
 export default antfu({
-
+  rules: {
+    'max-len': ['error', { code: 80 }],
+    'perfectionist/sort-imports': ['off'],
+    'import/order': [
+      'warn',
+      { groups: ['builtin', 'external'], warnOnUnassignedImports: true },
+    ],
+    'unused-imports/no-unused-imports': 'off',
+    '@typescript-eslint/consistent-type-definitions': 'off',
+    '@typescript-eslint/no-redeclare': 'off',
+    'vue/max-attributes-per-line': ['error', {
+      singleline: {
+        max: 3,
+      },
+      multiline: {
+        max: 1,
+      },
+    }],
+  },
 })
