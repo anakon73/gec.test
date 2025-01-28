539ed2a61289522c88d0203457ea300ab39ace13
anakon <nazarkinah73@gmail.com>
2025-01-28 20:51:28 +0200

    feat(GStar): impl

---
+++ src/shared/ui/GStar/GStar.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import { Star, StarHalf } from 'lucide-vue-next'
+
+export interface Props {
+  state: 'full' | 'half' | 'empty'
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+  <div class="relative">
+    <Star class="text-yellow size-[30px]" />
+    <StarHalf
+      v-if="state === 'half'"
+      class="text-yellow fill-yellow absolute top-0 size-[30px]"
+    />
+    <Star
+      v-else-if="state === 'full'"
+      class="text-yellow fill-yellow absolute top-0 size-[30px]"
+    />
+  </div>
+</template>
