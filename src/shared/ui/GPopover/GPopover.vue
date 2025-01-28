d2529d3b3e21038a7bdfc862f827b781ca51daa4
anakon <nazarkinah73@gmail.com>
2025-01-28 20:51:37 +0200

    feat(GPopover): impl

---
+++ src/shared/ui/GPopover/GPopover.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {
+  PopoverArrow,
+  PopoverContent,
+  PopoverPortal,
+  PopoverRoot,
+  PopoverTrigger,
+} from 'radix-vue'
+import type { VNode } from 'vue'
+
+export interface Slots {
+  default?: () => VNode[] | VNode | string
+  trigger?: () => VNode[] | VNode | string
+}
+
+defineSlots<Slots>()
+</script>
+
+<template>
+  <PopoverRoot>
+    <PopoverTrigger>
+      <slot name="trigger" />
+    </PopoverTrigger>
+    <PopoverPortal>
+      <PopoverContent
+        side="bottom"
+        :side-offset="5"
+        class="rounded p-5 w-72 bg-white popover-shadow"
+      >
+        <slot name="default" />
+        <PopoverArrow class="fill-white" />
+      </PopoverContent>
+    </PopoverPortal>
+  </PopoverRoot>
+</template>
---
+++ src/shared/ui/GPopover/index.ts
@@ -0,0 +1,4 @@
+export {
+  default as GPopover,
+  type Slots as GPopoverSlots,
+} from './GPopover.vue'
